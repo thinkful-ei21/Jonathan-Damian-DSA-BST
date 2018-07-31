3b2f15b7316761b9587c6d11d04b05928fa2f284
BuddhaL1T3 <jmriggs75@gmail.com>
2018-07-31 15:51:53 -0400

    done!

--- drills.js
+++ drills.js
@@ -30,53 +30,87 @@ const getHeight = BST => {
 
 //////////////////////////////////////////////////////
 
-const isBST = BST => {
-  //logic!
+const populateTree = () => {
+  const tree = new BinarySearchTree(5);
+  tree.left = new BinarySearchTree(7);
+  tree.right = new BinarySearchTree(4);
+  return tree;
+};
+
+//////////////////////////////////////////////////////
+
+const isBST = tree => {
+  if (tree.key === null) {
+    return true;
+  }
+  if (tree.left !== null) {
+    if (tree.left.key >= tree.key) {
+      return false;
+    }
+  }
+  if (tree.right !== null) {
+    if (tree.right.key <= tree.key) {
+      return false;
+    }
+  }
+  return true;
 };
 
 //////////////////////////////////////////////////////
 
 const findThirdLgKey = BST => {
-  //logic!
+  if (BST.key === null) {
+    return null;
+  }
+  const results = [];
+  const traverse = BST => {
+    if (BST.left) {
+      traverse(BST.left);
+    }
+    results.push(BST.key);
+    if (BST.right) {
+      traverse(BST.right);
+    }
+  };
+  traverse(BST);
+  return results[results.length - 3];
 };
 
 //////////////////////////////////////////////////////
 
 const balanceBST = BST => {
-  const minHeight = (node = this.key) => {
-    if (node === null) {
+  const minHeight = BST => {
+    if (BST.key === null) {
       return 0;
     }
-    let left = minHeight(node.left);
-    let right = minHeight(node.right);
-    if (left > right) {
-      return left + 1;
-    } else {
-      return right + 1;
-    }
+    let left = BST.left !== null ? minHeight(BST.left) : 0;
+    let right = BST.right !== null ? minHeight(BST.right) : 0;
+    return left < right ? left + 1 : right + 1;
   };
-  const maxHeight = (node = this.key) => {
-    if (node === null) {
+  const maxHeight = BST => {
+    if (BST.key === null) {
       return 0;
     }
-    let left = maxHeight(node.left);
-    let right = maxHeight(node.right);
-    if (left > right) {
-      return left + 1;
-    } else {
-      return right + 1;
-    }
+    let left = BST.left !== null ? maxHeight(BST.left) : 0;
+    let right = BST.right !== null ? maxHeight(BST.right) : 0;
+    return left > right ? left + 1 : right + 1;
   };
+  const diff = maxHeight(BST) - minHeight(BST);
+  return diff > 1 ? false : true;
 };
 
 //////////////////////////////////////////////////////
 
 const main = () => {
   const BST = populateBST();
+  const tree = populateTree();
 
   // console.log(BST);
-  console.log(getHeight(BST));
-  // console.log(isBST)
+  // console.log(getHeight(BST));
+  // console.log(tree);
+  // console.log(isBST(tree));
+  console.log(findThirdLgKey(BST));
+  // console.log(balanceBST(BST));
 };
 
 main();
